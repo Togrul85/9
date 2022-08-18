8ce88a984b6ffa8e40bc5e119ae9e06f95737feb
unknown <toghrul@code.edu.az>
2022-08-18 17:50:52 +0400

    .

---
+++ 3.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullname, groupNo;
+            int age;
+            do
+            {
+                Console.Write("Fullname:");
+                fullname = Console.ReadLine();
+            } while (!Student.CheckFullname(fullname));
+
+            do
+            {
+                Console.Write("Group No: ");
+                groupNo = Console.ReadLine();
+            } while (!Student.CheckGroupNo(groupNo));
+
+
+            do
+            {
+                Console.Write("age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+            } while (age <= 0 || age > 100);
+
+            Student st = new Student ( groupNo, fullname, age);
+
+        }
+    }
+}
---
+++ 7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    class Student
+    {
+        string _Fullname;
+        string _GroupNo;
+        string _Age;
+    }
+    public string Fullname
+    {
+        get { return _fullname; }
+        set { if (CheckFullname(value)) _fullname = value; }
+    }
+    public string GroupNo
+    {
+        get { return _groupNo; }
+        set { if (CheckGroupNo(value)) _groupNo = value; }
+    }
+
+}
